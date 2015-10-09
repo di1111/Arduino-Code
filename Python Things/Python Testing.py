485d86b18fd8ca8f68e77a58d3b7442140412bf2
di1111 <dylanhu2010@gmail.com>
2015-10-09 11:46:03 -0400

    More changes

--- Python Things/Python Testing.py
+++ Python Things/Python Testing.py
@@ -1,7 +1,10 @@
+'Import some stuff'
 import time
 
+'define all functions'
 def timep():
     print (time.asctime())
+'define all classes'
 
 print ("Hello! This is a test python file!")
 print ("The time right now is:")
