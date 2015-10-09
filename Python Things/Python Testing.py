866c6a496fa4e59cead9923756095f46591d5729
di1111 <dylanhu2010@gmail.com>
2015-10-09 11:48:59 -0400

    Test

--- Python Things/Python Testing.py
+++ Python Things/Python Testing.py
@@ -1,11 +1,14 @@
 'Import some stuff'
 import time
+import sys
 
 'define all functions'
 def timep():
     print (time.asctime())
 'define all classes'
 
+
 print ("Hello! This is a test python file!")
 print ("The time right now is:")
 timep()
+print ("What is your name?" + sys.stdin.readline())
