ff1790c643d3802d4c01f00694731e66ebd34fff
di1111 <dylanhu2010@gmail.com>
2015-10-09 12:35:10 -0400

    Changes

--- Python Things/Python Testing.py
+++ Python Things/Python Testing.py
@@ -6,9 +6,25 @@ import sys
 def timep():
     print (time.asctime())
 'define all classes'
+class people:
+    pass
+class name(people):
+    pass
+
+'define all variables'
 
 
 print ("Hello! This is a test python file!")
 print ("The time right now is:")
 timep()
-print ("What is your name?" + sys.stdin.readline())
+print ("What is your name?")
+username = sys.stdin.readline()
+print ("Hello " + username)
+print ("Do you want to regester you in the system? Y/N")
+yesno = sys.stdin.readline()
+if yesno == "Y":
+    print ("OK")
+    username = name()
+else:
+    print ("I won\'t add you")
+
