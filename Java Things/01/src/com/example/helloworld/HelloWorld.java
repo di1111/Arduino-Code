d84da34cc3eee59b7f1ae7517594369e7131dcf1
di1111 <dylanhu2010@gmail.com>
2015-10-30 12:29:32 -0400

    IDK

--- Java Things/01/src/com/example/helloworld/HelloWorld.java
+++ Java Things/01/src/com/example/helloworld/HelloWorld.java
@@ -7,6 +7,18 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
     }
 }
